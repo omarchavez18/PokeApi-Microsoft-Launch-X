d29f43b13c8135f2e2758d3dc7299548cf2f60a6
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-03-19 01:29:12 -0600

    creacion de etiquetas en html y codigo js

--- index.js
+++ index.js
@@ -9,24 +9,118 @@ const fetchPokemon = () => {
       if (res.status != "200") {
         console.log(res);
         pokeImage("./pikachu-llorando.gif");
+        `no se encontro`;
       } else {
         return res.json();
       }
     })
     .then((data) => {
       console.log(data);
+      //name
+      let name = data.species.name;
+      pokeNombre(name);
+      //id
+      let id = data.id;
+      pokeId(id);
+      //movimiento
+      let move = data.moves[0].move.name;
+      pokeMove(move);
+      //hp
+      let hp = data.stats[0].base_stat;
+      pokeHp(hp);
+      //attack
+      let attack = data.stats[1].base_stat;
+      pokeAttack(attack);
+      //defense
+      let defense = data.stats[2].base_stat;
+      pokeDefense(defense);
+      //specialAttack
+      specialAttack = data.stats[3].base_stat;
+      pokeSpecialAttack(specialAttack);
+      //special defense
+      let specialDefense = data.stats[4].base_stat;
+      pokeSpecialDefense(specialDefense);
+      //speed
+      let speed = data.stats[5].base_stat;
+      pokeSpeed(speed);
+      //type
+      let type = data.types[0].type.name;
+      pokeType(type);
+
+      //height
+      let height = data.height;
+      pokeHeight(height);
+
+      //weight
+      let weight = data.weight;
+      pokeWeight(weight);
+
+      //img
       let pokeImg = data.sprites.front_default;
-      console.log(pokeImg);
       pokeImage(pokeImg);
     });
 };
 
-//fetchPokemon();
+//
 
 const pokeImage = (url) => {
   const pokeImg = document.getElementById("pokeImg");
   pokeImg.src = url;
 };
-/*pokeImage(
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-);*/
+const pokeNombre = (name) => {
+  const pokeNombre = document.getElementById("pokeNombre");
+  pokeNombre.innerHTML = `Name = ${name.toUpperCase()}`;
+};
+
+const pokeId = (id) => {
+  const pokeId = document.getElementById("pokeId");
+  pokeId.innerHTML = `ID = ${id}`;
+};
+
+const pokeType = (type) => {
+  const pokeType = document.getElementById("pokeType");
+  pokeType.innerHTML = `Type = ${type}`;
+};
+
+const pokeSpeed = (speed) => {
+  const pokeSpeed = document.getElementById("pokeSpeed");
+  pokeSpeed.innerText = `Speed = ${speed}`;
+};
+
+const pokeSpecialDefense = (specialDefense) => {
+  const pokeSpecialDefense = document.getElementById("pokeSDefense");
+  pokeSpecialDefense.innerHTML = `Special defense = ${specialDefense}`;
+};
+
+const pokeWeight = (weight) => {
+  const pokeWeight = document.getElementById("pokeWeight");
+  pokeWeight.innerHTML = `Weight = ${weight} `;
+};
+
+const pokeHeight = (height) => {
+  const pokeHeight = document.getElementById("pokeHeight");
+  pokeHeight.innerHTML = `Height = ${height} `;
+};
+
+const pokeSpecialAttack = (specialAttack) => {
+  const pokeSpecialAttack = document.getElementById("pokeSAttack");
+  pokeSpecialAttack.innerHTML = `Special attack = ${specialAttack}`;
+};
+
+const pokeMove = (move) => {
+  const pokeMove = document.getElementById("pokeMove");
+  pokeMove.innerHTML = `Move = ${move}`;
+};
+
+const pokeHp = (hp) => {
+  const pokeHp = document.getElementById("pokeHp");
+  pokeHp.innerHTML = `Hp = ${hp}`;
+};
+const pokeAttack = (attack) => {
+  const pokeAttack = document.getElementById("pokeAttack");
+  pokeAttack.innerHTML = `Attack = ${attack}`;
+};
+const pokeDefense = (defense) => {
+  const pokeDefense = document.getElementById("pokeDefense");
+  pokeDefense.innerHTML = `Defense = ${defense}`;
+};
